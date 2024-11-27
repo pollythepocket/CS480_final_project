c87f9e9d06a78cb867e8ad9760ef56ebbff73431
Ajknight121 <aknig2@uic.edu>
2024-11-27 15:52:15 -0600

    Send when client not found

--- frontend-music-app/src/domain/siteContext.jsx
+++ frontend-music-app/src/domain/siteContext.jsx
@@ -4,10 +4,11 @@ import { createContext, useState } from "react";
 export const siteContext = createContext();
 
 export default function SiteContextProvider({children}) {
+  const [username, setUsername] = useState("")
   const [songs, setSongs] = useState([]);
   const [users, setUsers] = useState([]);
   return (
-    <siteContext.Provider value={{songs, users, setSongs, setUsers}}>
+    <siteContext.Provider value={{songs, users, setSongs, setUsers, username, setUsername}}>
       {children}
     </siteContext.Provider>
   )
