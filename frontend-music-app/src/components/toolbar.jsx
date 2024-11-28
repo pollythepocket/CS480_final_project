e7d4ca8e55caf3f60a62ad08faf318c209a876c1
pollythepocket <pollyruhnke@gmail.com>
2024-11-27 20:38:46 -0600

    getting things started

--- frontend-music-app/src/components/toolbar.jsx
+++ frontend-music-app/src/components/toolbar.jsx
@@ -21,12 +21,18 @@ export default function Toolbar({username}) {
         let path = '/login';
         navigate(path, {state: username});
       } 
+
+      const handlePostSong = () => {
+        let path = '/PostSong';
+        navigate(path, {state: username});
+      }
   
 
   return (
     <div className="toolbar">
           <button type="submit" className="taskbar-button" onClick={routeChange}>Home</button>
           <button type="submit" className="taskbar-button" onClick={routeChangeToLikedList}>Liked Songs</button>
+          <button type="submit" className="taskbar-button" onClick={handlePostSong}>Post Song</button>
           <button type="submit" className="taskbar-button" onClick={routeChangeToLogin}>Logout</button>
     </div>
   );
